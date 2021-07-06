cfc818cdcd4a6735a00eaee33eab93bf64bc2af6
sykang <sykang@inspace.re.kr>
2021-07-06 16:09:54 +0900

    update schedule layout

--- client/src/views/MySchedule/MySchedule.js
+++ client/src/views/MySchedule/MySchedule.js
@@ -84,6 +84,117 @@ export default function MySchedule() {
     }
   }, [groups]);
 
+  /**************************************************** */
+  /**********************검색 관련********************** */
+  /**************************************************** */
+  // toggle button 클릭
+  const handleToggle = (e, newMode) => {
+    setViewMode(newMode);
+    setSelectedGroupIds([]);
+    setSelectedUserIds([]);
+  };
+
+  // viewMode=users에서 그룹 select option 변경
+  const handleGroupSelectChange = (e) => {
+    const selectId = e.target.value;
+    // if All group
+    if (selectId === "") {
+      setSelectedGroup(groupInitState); // groupInitState의 id는 ""이므로 searchSchedule에서 전체 조회가 가능
+      setSelectedUserIds([]);
+    } else {
+      // 선택한 그룹 setSeletedGroup에 저장
+      const selectGroup = groups.find((x) => x.id === selectId);
+      setSelectedGroup(selectGroup);
+      // 그룹 변경 시 기본적으로 해당 그룹 멤버 전체 선택
+      const selectUserIds = selectGroup.users && selectGroup.users.map((obj) => obj.id);
+      setSelectedUserIds(selectUserIds);
+    }
+  };
+
+  // viewMode=users에서 사용자 전체 체크박스 클릭
+  const handleUserAllCheckbox = (e) => {
+    const checked = e.target.checked;
+    if (checked) {
+      const selectUserIds = selectedGroup.users && selectedGroup.users.map((obj) => obj.id);
+      setSelectedUserIds(selectUserIds);
+    } else {
+      setSelectedUserIds([]);
+    }
+  };
+
+  // viewMode=groups에서 그룹 전체 체크박스 클릭
+  const handleGroupAllCheckbox = (e) => {
+    const checked = e.target.checked;
+    if (checked) {
+      // group all이 체크되어 있으면 모든 그룹의 모든 유저들을 setSelectedUserIds에 삽입
+      const selectedGroupIds = groups.map((obj) => obj.id);
+      setSelectedGroupIds(selectedGroupIds);
+      let userIdArr = [];
+      for (let idx in groups) {
+        const users = groups[idx].users;
+        const userIds = users.map((obj) => obj.id);
+        userIdArr = [...userIdArr, ...userIds];
+      }
+      setSelectedUserIds(userIdArr);
+    } else {
+      setSelectedGroupIds([]);
+      setSelectedUserIds([]);
+    }
+  };
+
+  // viewMode=users에서 사용자 체크박스 클릭
+  const handleUserCheckbox = (e) => {
+    const checkedId = parseInt(e.target.value);
+    const checked = e.target.checked;
+    if (checked) {
+      // checkbox가 체크되었으면 userIds에 추가
+      setSelectedUserIds([...selectedUserIds, checkedId]);
+    } else {
+      // checkbox가 해제되었으면 userIds에서 삭제
+      setSelectedUserIds(selectedUserIds.filter((id) => id !== checkedId));
+    }
+  };
+
+  // viewMode=groups에서 그룹 체크박스 클릭
+  const handleGroupCheckbox = (e) => {
+    const checkedId = parseInt(e.target.value);
+    const checked = e.target.checked;
+
+    const checkedGroup = groups.find((x) => x.id === checkedId);
+    const users = checkedGroup.users;
+    const userIds = users.map((obj) => obj.id);
+    if (checked) {
+      setSelectedGroupIds([...selectedGroupIds, checkedId]);
+      // checkbox가 체크되었으면 체크된 그룹의 user들을 selectedUserIds에 추가
+      setSelectedUserIds([...selectedUserIds, ...userIds]);
+    } else {
+      setSelectedGroupIds(selectedGroupIds.filter((id) => id !== checkedId));
+      // checkbox가 해제되었으면 해제된 그룹의 user들을 selectedUserIds에서 찾아서 삭제
+      setSelectedUserIds(selectedUserIds.filter((x) => userIds.indexOf(x) < 0));
+    }
+  };
+
+  // viewMode=users에서 선택한 사용자에 따른 스케줄 목록 재조회
+  const searchSchedule = (users, groups) => {
+    let idParam = users.join(",");
+    // All group이 선택되어 있는 경우 모든 스케줄을 표출
+    if (groups.id === "") {
+      dispatch(retrieveSchedules());
+    } else {
+      // 선택된 사용자가 없는 경우
+      if (idParam === "") {
+        idParam = "[]";
+      }
+      const params = {
+        userIdsStr: idParam,
+      };
+      dispatch(retrieveSchedules(params));
+    }
+  };
+
+  /**************************************************** */
+  /*******************캘린더 내부 관련****************** */
+  /**************************************************** */
   // 날짜 클릭 시 신규 스케줄 추가 팝업 오픈
   const handleDateSelect = (e) => {
     setClickedDate(e.start);
@@ -266,143 +377,11 @@ export default function MySchedule() {
     setEditScheduleModalOpen(value);
   };
 
-  // toggle button 클릭
-  const handleToggle = (e, newMode) => {
-    setViewMode(newMode);
-    setSelectedGroupIds([]);
-    setSelectedUserIds([]);
-  };
-
-  // viewMode=users에서 그룹 select option 변경
-  const handleGroupSelectChange = (e) => {
-    const selectId = e.target.value;
-    // if All group
-    if (selectId === "") {
-      setSelectedGroup(groupInitState); // groupInitState의 id는 ""이므로 searchSchedule에서 전체 조회가 가능
-      setSelectedUserIds([]);
-    } else {
-      // 선택한 그룹 setSeletedGroup에 저장
-      const selectGroup = groups.find((x) => x.id === selectId);
-      setSelectedGroup(selectGroup);
-      // 그룹 변경 시 기본적으로 해당 그룹 멤버 전체 선택
-      const selectUserIds = selectGroup.users && selectGroup.users.map((obj) => obj.id);
-      setSelectedUserIds(selectUserIds);
-    }
-  };
-
-  // viewMode=users에서 사용자 전체 체크박스 클릭
-  const handleUserAllCheckbox = (e) => {
-    const checked = e.target.checked;
-    if (checked) {
-      const selectUserIds = selectedGroup.users && selectedGroup.users.map((obj) => obj.id);
-      setSelectedUserIds(selectUserIds);
-    } else {
-      setSelectedUserIds([]);
-    }
-  };
-
-  // viewMode=groups에서 그룹 전체 체크박스 클릭
-  const handleGroupAllCheckbox = (e) => {
-    const checked = e.target.checked;
-    if (checked) {
-      // group all이 체크되어 있으면 모든 그룹의 모든 유저들을 setSelectedUserIds에 삽입
-      const selectedGroupIds = groups.map((obj) => obj.id);
-      setSelectedGroupIds(selectedGroupIds);
-      let userIdArr = [];
-      for (let idx in groups) {
-        const users = groups[idx].users;
-        const userIds = users.map((obj) => obj.id);
-        userIdArr = [...userIdArr, ...userIds];
-      }
-      setSelectedUserIds(userIdArr);
-    } else {
-      setSelectedGroupIds([]);
-      setSelectedUserIds([]);
-    }
-  };
-
-  // viewMode=users에서 사용자 체크박스 클릭
-  const handleUserCheckbox = (e) => {
-    const checkedId = parseInt(e.target.value);
-    const checked = e.target.checked;
-    if (checked) {
-      // checkbox가 체크되었으면 userIds에 추가
-      setSelectedUserIds([...selectedUserIds, checkedId]);
-    } else {
-      // checkbox가 해제되었으면 userIds에서 삭제
-      setSelectedUserIds(selectedUserIds.filter((id) => id !== checkedId));
-    }
-  };
-
-  // viewMode=groups에서 그룹 체크박스 클릭
-  const handleGroupCheckbox = (e) => {
-    const checkedId = parseInt(e.target.value);
-    const checked = e.target.checked;
-
-    const checkedGroup = groups.find((x) => x.id === checkedId);
-    const users = checkedGroup.users;
-    const userIds = users.map((obj) => obj.id);
-    if (checked) {
-      setSelectedGroupIds([...selectedGroupIds, checkedId]);
-      // checkbox가 체크되었으면 체크된 그룹의 user들을 selectedUserIds에 추가
-      setSelectedUserIds([...selectedUserIds, ...userIds]);
-    } else {
-      setSelectedGroupIds(selectedGroupIds.filter((id) => id !== checkedId));
-      // checkbox가 해제되었으면 해제된 그룹의 user들을 selectedUserIds에서 찾아서 삭제
-      setSelectedUserIds(selectedUserIds.filter((x) => userIds.indexOf(x) < 0));
-    }
-  };
-
-  // viewMode=users에서 선택한 사용자에 따른 스케줄 목록 재조회
-  const searchSchedule = (users, groups) => {
-    let idParam = users.join(",");
-    // All group이 선택되어 있는 경우 모든 스케줄을 표출
-    if (groups.id === "") {
-      dispatch(retrieveSchedules());
-    } else {
-      // 선택된 사용자가 없는 경우
-      if (idParam === "") {
-        idParam = "[]";
-      }
-      const params = {
-        userIdsStr: idParam,
-      };
-      dispatch(retrieveSchedules(params));
-    }
-  };
-
   return (
     <>
       <GridContainer>
-        <GridItem xs={12} sm={12} md={12}>
-          <FullCalendar
-            plugins={[dayGridPlugin, rrulePlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-              left: "prev,next today",
-              center: "title",
-              right: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            initialView="dayGridMonth"
-            editable={true}
-            selectable={true}
-            selectMirror={true}
-            dayMaxEvents={true}
-            weekends={true}
-            fixedWeekCount={false}
-            select={handleDateSelect}
-            events={schedules}
-            eventClick={handleEventClick}
-            eventAdd={handleEventAdd}
-            eventChange={handleEventChange} // called for drag-n-drop/resize
-            eventRemove={handleEventRemove}
-            eventContent={handleEventContent}
-          />
-        </GridItem>
-      </GridContainer>
-      <br />
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={1} style={{ marginRight: "20px" }}>
-          <ToggleButtonGroup size="small" value={viewMode} exclusive onChange={handleToggle} aria-label="text alignment">
+        <GridItem xs={12} sm={12} md={1}>
+          <ToggleButtonGroup size="small" value={viewMode} exclusive onChange={handleToggle} aria-label="text alignment" style={{ height: "34px" }}>
             <ToggleButton value="users" aria-label="left aligned">
               <p>Users</p>
             </ToggleButton>
@@ -428,7 +407,7 @@ export default function MySchedule() {
                 </Select>
               </FormControl>
             </GridItem>
-            <GridItem xs={12} sm={12} md={8}>
+            <GridItem xs={12} sm={12} md={9}>
               {selectedGroup.users.length > 0 ? (
                 <FormControlLabel
                   value=""
@@ -510,6 +489,34 @@ export default function MySchedule() {
             </GridItem>
           </>
         )}
+      </GridContainer>
+      <hr />
+      <br />
+      <GridContainer>
+        <GridItem xs={12} sm={12} md={12}>
+          <FullCalendar
+            plugins={[dayGridPlugin, rrulePlugin, timeGridPlugin, interactionPlugin]}
+            headerToolbar={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay",
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            weekends={true}
+            fixedWeekCount={false}
+            select={handleDateSelect}
+            events={schedules}
+            eventClick={handleEventClick}
+            eventAdd={handleEventAdd}
+            eventChange={handleEventChange} // called for drag-n-drop/resize
+            eventRemove={handleEventRemove}
+            eventContent={handleEventContent}
+          />
+        </GridItem>
       </GridContainer>
 
       <AddScheduleForm open={addScheduleModalOpen} handleCloseClick={handleAddScheduleModalClick} date={clickedDate} />
