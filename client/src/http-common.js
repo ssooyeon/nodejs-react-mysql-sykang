3a139b016d76ded2157cf6cee746d6bd9ec2eebc
sykang <sykang@inspace.re.kr>
2021-07-22 15:18:37 +0900

    test#5

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8081/api",
-  baseURL: "/api",
+  baseURL: "https://nodejs-react-mysql-sykang.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
