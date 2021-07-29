56f1ef00b7b39ea32b59f6f97e008a402a49a5f8
sykang <sykang@inspace.re.kr>
2021-07-29 10:47:51 +0900

    .

--- server.js
+++ server.js
@@ -21,24 +21,27 @@ const db = require("./models");
 // db.sequelize.sync({ force: true });  // DB 테이블 초기화 옵션
 db.sequelize.sync();
 
-// heroku deploy일 경우에만 client build 설정
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("/", (req, res) => {
-    console.log("==========================>test from app.get!");
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-  app.get("/test", (req, res) => {
-    console.log("==========================>test from app.get.test!");
-    res.send("test route page");
-  });
-
-  // heroku server를 9(utc:0)-19(utc:10)시에 20분마다 호출
-  cron.schedule("*/20 0-10 * * *", function () {
-    console.log(`node-cron: call ${process.env.DEPLOY_SERVER_URL}`);
-    http.get(process.env.DEPLOY_SERVER_URL);
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// heroku deploy일 경우에만 client build 설정
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client", "build")));
+//   app.get("/", (req, res) => {
+//     console.log("==========================>test from app.get!");
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+//   });
+
+//   // heroku server를 9(utc:0)-19(utc:10)시에 20분마다 호출
+//   cron.schedule("*/20 0-10 * * *", function () {
+//     console.log(`node-cron: call ${process.env.DEPLOY_SERVER_URL}`);
+//     http.get(process.env.DEPLOY_SERVER_URL);
+//   });
+// }
 
 // routes
 require("./routes/user.routes")(app);
