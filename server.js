f05f27ea3dc22bcdbec62df56453f2d87dfb73b6
sykang <sykang@inspace.re.kr>
2021-07-29 10:57:11 +0900

    .

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.sequelize.sync();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
